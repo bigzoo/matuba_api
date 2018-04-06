e62a828fe54c88234d67be6e439248723fb1ab07
Your Name <you@example.com>
2018-04-07 01:35:13 +0300

    Created migration for users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('nickname')->nullable();
+            $table->string('name')->nullable();
+            $table->string('phone_no')->nullable();
+            $table->string('county')->nullable();
             $table->string('email')->unique();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
