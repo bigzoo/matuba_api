c66ff9717c2f445348c50fc7be6b9da163f6a740
Chris Nyaga <nyagachris75@gmail.com>
2018-04-07 04:32:21 +0300

    Created migration, model, resource, api controller for reports with relation to user.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
-    $api->resource('user','App\Http\Controllers\Api\UserController', ['except' => ['index']]);
+    $api->resource('users', 'App\Http\Controllers\Api\UserController', ['except' => ['index']]);
+    $api->resource('reports', 'App\Http\Controllers\Api\ReportsController');
 });
 
