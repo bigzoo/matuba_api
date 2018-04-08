d1ed43a6b44b4b27ef0cb6ade18704f46a3f68bd
Chris <nyagachris75@gmail.com>
2018-04-08 04:45:51 +0300

    Added a get for journeys.

--- app/Http/Controllers/JourneysController.php
+++ app/Http/Controllers/JourneysController.php
@@ -66,6 +66,64 @@ class JourneysController extends Controller
         ];
     }
 
+    public function showGet(Request $request)
+    {
+        $wimt_api = new WimtApi();
+        $from_lat = $request->get('from_lat');
+        $from_long = $request->get('from_long');
+        $to_lat = $request->get('to_lat');
+        $to_long = $request->get('to_long');
+        $maxItineraries = $request->get('maxItineraries', 5);
+        $journey = $wimt_api->getJourney(
+            ['lat' => $from_lat, 'long' =>$from_long],
+            ['lat' => $to_lat, 'long' => $to_long],
+            $maxItineraries
+        );
+        $itineraries = collect($journey->itineraries)->map(function ($itenerary){
+            $legs = collect($itenerary->legs)->map(function ($leg){
+                $waypoints = collect($leg->waypoints)->map(function ($waypoint){
+                    return [
+                        'location_address' => $waypoint->location->address,
+                        'location_coordinates' => $waypoint->location->geometry->coordinates,
+                        'departureTime' => $this->parseDateToString($waypoint->departureTime),
+                        'arrivalTime' => $this->parseDateToString($waypoint->arrivalTime),
+                    ];
+                });
+                $directions = collect($leg->directions)->map(function ($direction){
+                    return [
+                        'instruction' => $direction->instruction,
+                        'distance' => $direction->distance->value.$direction->distance->unit
+                    ];
+                });
+                return [
+                    'type' => $leg->type,
+                    'behaviour' => $leg->behaviour,
+                    'distance' => $leg->distance->value.$leg->distance->unit,
+                    'waypoints' => $waypoints,
+                    'directions' => $directions
+                ];
+            });
+            return [
+                'departure_time' => $this->parseDateToString($itenerary->departureTime),
+                'arrival_time' => $this->parseDateToString($itenerary->arrivalTime),
+                'total_distance' => $itenerary->distance->value.$itenerary->distance->unit,
+                'travel_time' => $itenerary->duration,
+                'legs' => $legs
+            ];
+        });
+        return [
+            'from' => [
+                'latitude' => $journey->geometry->coordinates[0][0],
+                'longitude' => $journey->geometry->coordinates[0][1]
+            ],
+            'to' => [
+                'latitude' => $journey->geometry->coordinates[1][0],
+                'longitude' => $journey->geometry->coordinates[1][1]
+            ],
+            'itineraries' => $itineraries
+        ];
+    }
+
     private function parseDateToString($date)
     {
         return Carbon::parse($date)->toDateTimeString();
--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,5 @@ $api->version('v1', function ($api) {
     $api->resource('users', 'App\Http\Controllers\Api\UserController', ['except' => ['index']]);
     $api->resource('reports', 'App\Http\Controllers\Api\ReportsController');
     $api->post('journeys', 'App\Http\Controllers\JourneysController@show');
+    $api->get('journeys', 'App\Http\Controllers\JourneysController@showGet');
 });
