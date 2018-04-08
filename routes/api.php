9654e8471e47d417cd61ec6fe9f0295c119ac694
Chris <nyagachris75@gmail.com>
2018-04-08 04:16:39 +0300

    Created endpoint for querying journey.

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,5 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', function ($api) {
     $api->resource('users', 'App\Http\Controllers\Api\UserController', ['except' => ['index']]);
     $api->resource('reports', 'App\Http\Controllers\Api\ReportsController');
+    $api->post('journeys', 'App\Http\Controllers\JourneysController@show');
 });
